a2a99613fa7fce098da589ab6a8ad721c03d10a8
Christel Chan <christelyhchan@gmail.com>
2017-11-22 15:06:59 -0800

    doneee

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import WeatherDisplay from './WeatherDisplay.js';
+import sunnyIcon from './assets/sunny-icon.svg';
+import rainyIcon from './assets/rain-icon.svg';
+import cloudyIcon from './assets/cloud-icon.svg';
+import windyIcon from './assets/windy-icon.svg';
 
 class WeatherApp extends Component {
   constructor (props) {
@@ -23,9 +27,25 @@ class WeatherApp extends Component {
           const tempInF = response.data.query.results.channel.item.condition.temp
           const tempInC = Math.round((tempInF - 32)/1.8)
 
+          // if function for rendering icons
+          const weather = response.data.query.results.channel.item.condition.text;
+
+          if (weather.includes('Sunny')) {
+            var icon = sunnyIcon
+          } else if (weather.includes('Showers')) {
+            var icon = rainyIcon
+          } else if (weather.includes('Cloudy')) {
+            var icon = cloudyIcon
+          } else if (weather.includes('Breezy')) {
+            var icon = windyIcon
+          } else {
+            var icon = cloudyIcon
+          }
+
           this.setState({
             temp: tempInC,
-            weather: response.data.query.results.channel.item.condition.text
+            weather: weather,
+            icon: icon
           })
         })
     });
@@ -41,11 +61,11 @@ class WeatherApp extends Component {
         <p>Select a city to view its current weather</p>
         <select value={this.state.value} onChange={this.handleChange}>
           <option value='none'>Select a City</option>
-          <option value='Vancouver, BC'>Vancouver, Canada</option>
-          <option value='Hong Kong, HK'>Hong Kong, HK</option>
-          <option value='Sydney, Australia'>Sydney, Australia</option>
-          <option value='London, UK'>London, UK</option>
-          <option value='New York, USA'>New York, USA</option>
+          <option value='Vancouver'>Vancouver, Canada</option>
+          <option value='Paris'>Paris, France</option>
+          <option value='Sydney'>Sydney, Australia</option>
+          <option value='London'>London, UK</option>
+          <option value='New York'>New York, USA</option>
         </select>
 
         {/* Only render component if a city was selected */}
@@ -54,6 +74,7 @@ class WeatherApp extends Component {
             city = {this.state.value}
             temp = {this.state.temp}
             weather = {this.state.weather}
+            icon = {this.state.icon}
           />
         }
       </div>
--- src/WeatherDisplay.js
+++ src/WeatherDisplay.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 
 class WeatherDisplay extends Component {
 
-  render() {
+  render () {
+
+    var weather = this.props.weather;
     return (
       <div className = 'weather-display'>
         <h2>{this.props.city}</h2>
-        <h3 className = 'temp'>{this.props.temp}&deg;C</h3>
+        <h3 className = 'temp'>{this.props.temp}&deg;</h3>
+        <img src={this.props.icon} alt={this.props.weather} className='weather-icon'/>
         <h3 className = 'weather'>{this.props.weather}</h3>
       </div>
 
