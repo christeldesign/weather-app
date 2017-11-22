c7aadeee6f854e049673cd47b85e695df813c80b
Christel Chan <christelyhchan@gmail.com>
2017-11-22 12:40:27 -0800

    basically the whole app lol..

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -3,55 +3,56 @@ import axios from 'axios';
 import WeatherDisplay from './WeatherDisplay.js';
 
 class WeatherApp extends Component {
-  constructor(props){
+  constructor (props) {
     super(props);
-    this.state = { value: '' };
+    this.state = { value: 'none' };
 
     this.handleChange = this.handleChange.bind(this);
   }
 
   // Event handler to handle change on selected cities
-  handleChange(event) {
-      // Set chosen city in state
-      this.setState({ value: event.target.value}, () => {
-        // get weather info
-        axios.get(`https://query.yahooapis.com/v1/public/yql?format=json&q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='${this.state.value}')`)
-             .then((response) => {
-               // Information needed stored in response.data.query.results.channel.item.condition
+  handleChange (event) {
+    // Set chosen city in state
+    this.setState({ value: event.target.value }, () => {
+      // get weather info
+      axios.get(`https://query.yahooapis.com/v1/public/yql?format=json&q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='${this.state.value}')`)
+           .then((response) => {
+          // Information needed stored in response.data.query.results.channel.item.condition
 
-               // Convert temperature to Celcius
-               const tempInF = response.data.query.results.channel.item.condition.temp
-               const tempInC = Math.round((tempInF - 32)/1.8)
+          // Convert temperature to Celcius
+          const tempInF = response.data.query.results.channel.item.condition.temp
+          const tempInC = Math.round((tempInF - 32)/1.8)
 
-               this.setState({
-                 temp: tempInC,
-                 weather: response.data.query.results.channel.item.condition.text
-                })
-             })
-      });
+          this.setState({
+            temp: tempInC,
+            weather: response.data.query.results.channel.item.condition.text
+          })
+        })
+    });
   }
 
-  render() {
+  render () {
 
     return (
       <div>
         <h1>Weather App</h1>
+        <p>Select a city to view its current weather</p>
         <select value={this.state.value} onChange={this.handleChange}>
-          <option>Select a City</option>
+          <option value='none'>Select a City</option>
           <option value='Vancouver, BC'>Vancouver, Canada</option>
-          <option value='Hong Kong, HK'>Hong Kong, China</option>
-          <option value='Sydney'>Sydney</option>
-          <option value='London'>London</option>
-          <option value='New York'>New York</option>
+          <option value='Hong Kong, HK'>Hong Kong, HK</option>
+          <option value='Sydney, Australia'>Sydney, Australia</option>
+          <option value='London, UK'>London, UK</option>
+          <option value='New York, USA'>New York, USA</option>
         </select>
 
         {/* Only render component if a city was selected */}
-        {this.state.value != '' &&
-        <WeatherDisplay
-          city = {this.state.value}
-          temp = {this.state.temp}
-          weather = {this.state.weather}
-        />
+        {this.state.value !== 'none' &&
+          <WeatherDisplay
+            city = {this.state.value}
+            temp = {this.state.temp}
+            weather = {this.state.weather}
+          />
         }
       </div>
     );
--- src/WeatherDisplay.js
+++ src/WeatherDisplay.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 
 class WeatherDisplay extends Component {
-  constructor(props){
-    super(props);
-  }
 
   render() {
-
     return (
       <div className = 'weather-display'>
-        <h2>Weather for {this.props.city}</h2>
-        <h3>{this.props.temp}</h3>
-        <h3>{this.props.weather}</h3>
+        <h2>{this.props.city}</h2>
+        <h3 className = 'temp'>{this.props.temp}&deg;C</h3>
+        <h3 className = 'weather'>{this.props.weather}</h3>
       </div>
 
     );
